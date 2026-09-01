e0d486d9ad1435d37167b141300e6815258a36e7
srishti <mishtigangwar11@gmail.com>
2026-09-01 19:08:47 +0530

    Time: 3 ms (34.27%) | Memory: 63 MB (49.27%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,18 +1,21 @@
 class Solution {
-    public:
-        int maxArea(vector<int>& height) {
-                int maxArea = 0;
-                        int left = 0;
-                                int right = height.size()-1;
-                                         while(left<right) {
-                                                     maxArea = max(maxArea,(right-left)*min(height[left], height[right]));
-                                                                 if(height[left]<height[right]) {
-                                                                                 left++;
-                                                                                             }
-                                                                                                         else{
-                                                                                                                         right--;
-                                                                                                                                     }
-                                                                                                                                              }
-                                                                                                                                                       return maxArea;
-                                                                                                                                                           }
-                                                                                                                                                           };
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0;
+        int right = height.size()-1;
+        int max_area = 0;
+         while(left<right) {
+            int h = min(height[left], height[right]);
+            int w = right - left;
+            max_area = max(max_area, h * w);
+        
+         if(height[left]< height[right])
+            left++;
+         
+         else
+            right--;
+         }
+         
+         return max_area;
+         }
+};

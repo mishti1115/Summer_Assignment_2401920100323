2802bf45a1a6404d47a2b91cb4c46b3d51b10c75
srishti <mishtigangwar11@gmail.com>
2026-09-14 23:25:55 +0530

    Time: 1058 ms (5.02%) | Memory: 60.5 MB (7.88%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int map[26] = {0};
-        for(char c : s1) map[c - 'a']++;
-        int j = 0, i = 0, count_chars = s1.size();
-        while(j<s2.size()) {
-            if(map[s2.at(j++) - 'a']-- > 0)
-            count_chars--;
-            if(count_chars == 0) return true;
-            if(j - i == s1.size() && map[s2.at(i++) - 'a']++ >=0)
-            count_chars++;
+        int n = s1.size();
+        int m = s2.size();
+
+        if (n > m) return false;
+
+        sort(s1.begin(), s1.end());
+
+        for (int i = 0; i <= m - n; i++) {
+            string temp = s2.substr(i, n);
+
+            sort(temp.begin(), temp.end());
+
+            if (temp == s1)
+                return true;
         }
+
         return false;
     }
 };
